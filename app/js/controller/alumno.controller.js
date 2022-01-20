c173e76f4a6652de52707cadcede92f245fff60e
Karlinkd010 <karlinkd010@gmail.com>
2022-01-20 13:52:15 -0600

    [UPDATE]

--- app/js/controller/alumno.controller.js
+++ app/js/controller/alumno.controller.js
@@ -1,7 +1,17 @@
 var app= angular.module("index-template");
 
-    app.controller("alumno-ver-controller",function($scope,$http){
+    app.controller("alumno-ver-controller",function($scope,$routeParams,$http){
 
-        $scope.mensaje="JDBD";
+        var codigo=$routeParams.codigo;
+        $scope.alumno={};
+        $http.get("https://localhost:44367/api/Alumno/Busca?idalumno="+codigo)
+            .then(function(data){
+                
+                $scope.alumno=data.data;
+                console.log($scope.alumno);
+            },
+            function(err){
+                console.log(err);
+        });
         
     });
